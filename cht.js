23a822c111d2cf907aeee1335124c51158bc3d96
PIXINK® <100534677+pix-ink@users.noreply.github.com>
2024-02-28 04:11:31 +1300

    Create cht.js

---
+++ cht.js
@@ -0,0 +1,3 @@
+var chtDiv = document.createElement('div');
+chtDiv.innerHTML = '<div id="fb-root"></div><div class="fb-customerchat" attribution=setup_tool page_id="307319856020672" theme_color="#0080bc"></div>';
+document.body.appendChild(chtDiv);
